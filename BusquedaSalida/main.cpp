10d9b1f6cf287a55d9373b6a4f744162137f959b
Luis Méndez <44847230+mndzl@users.noreply.github.com>
2019-07-30 15:51:36 -0300

    Final version w/ map view

--- BusquedaSalida/main.cpp
+++ BusquedaSalida/main.cpp
@@ -1,77 +1,117 @@
 #include <bits/stdc++.h>
-#include <fstream>
-#define in 1
-#define out 2
-#define closed 3
-#define free 4
-#define visited 5
 using namespace std;
 
-int mapa[6][6];
-queue<pair<int,int>>q;
-pair<int,int>salida;
-bool outFinded=false;
-void bfs(int x, int y);
+int h=8,w=8;
+char adj[100][100];
 
-int pasos=0;
+struct Estado{
+	int x;
+	int y;
+	int d;
+	Estado(int x2,int y2,int d2):x(x2),y(y2),d(d2){};
+};
+
+int bfs(int x, int y);
+void makePath(pair<int,int>salida,vector<vector<char>>prev);
 
 int main(){
 ifstream e("entrada.in");
-	
-	// Creacion del mapa
-	int i=-1;
-	pair<int,int>entrada;
-	
-	do{
-		i++;
-			for(int j=0; j<6; j++){
-				char a;
-				e>>a;
-				switch(a){
-					case 'I':mapa[i][j]=in;entrada=make_pair(i,j);break;
-					case 'S':mapa[i][j]=out;break;
-					case '#':mapa[i][j]=closed;break;
-					case '-':mapa[i][j]=free;break;
-				}
+
+   
+	int x=-1;
+	int y=-1;
+	// Carga de laberinto 
+	for(int i=0; i<h; i++){
+		for(int j=0; j<w; j++){
+			e>>adj[i][j];
+			if(adj[i][j]=='I'){
+				x=i;
+				y=j; 
 			}
-	}while(i<5);
-
-	
-	// Algoritmo de búsqueda BFS
-	
-	q.push({entrada.first,entrada.second});
-	pair<int,int>actual;
-	mapa[entrada.first][entrada.second]=visited;
-	int dx[]={-1,1,0,0};
-	int dy[]={0,0,-1,1};
-
-	while(!q.empty()){
-		actual = q.front();
-		q.pop();
-		int x = actual.first;
-		int y = actual.second;
-		pasos++;
-
-		for(int z=0; z<4; z++){
-			int nx = x+dx[z];
-			int ny = y+dy[z];
-			if(nx>=0 && nx<6 && ny>=0 && ny<6)bfs(nx,ny);
-			if(outFinded)return 0;
 		}
 	}
+	cout<<"Pasos mínimos: "<<bfs(x,y)<<endl;
+	for(int i=0; i<h; i++){
+		for(int j=0; j<w; j++){
+			cout<<adj[i][j];
+		}
+		cout<<"\n";
+	}
 	
-	cout<<"No se ha encontrado una salida";
-	
-e.close();	
+	e.close();
 	return 0;
 }
-void bfs(int x, int y){
-	if(mapa[x][y]==out){
-		outFinded=true;
-		cout<<"Salida encontrada en: "<<x<<" "<<y<<" en "<<pasos<<" pasos";
-	}else
-	if(mapa[x][y]==free){
-		q.push({x,y});
-		mapa[x][y]=visited;
+
+void makePath(pair<int,int>salida,vector<vector<char>>prev,int pasos){
+	pair<int,int>a=salida;
+	pair<int,int>vecino;
+	for(int i=0; i<pasos-1; i++){
+		switch(prev[a.first][a.second]){
+			case 'a':vecino = make_pair(a.first+1,a.second);break;
+			case 'A':vecino = make_pair(a.first-1,a.second);break;
+			case 'd':vecino = make_pair(a.first,a.second+1);break;
+			case 'i':vecino = make_pair(a.first,a.second-1);break;
+			case 'n':vecino = make_pair(-1,-1);break;
+
+		}
+		adj[vecino.first][vecino.second]='*';
+		a = vecino;
 	}
+}
+
+int bfs(int x,int y){
+	vector<vector<char>>prev;
+	prev.resize(8);
+	for(int i=0; i<8; i++){
+		prev[i].resize(8);
+	}
+	for(int i=0; i<8; i++){
+		for(int j=0; j<8; j++){
+			prev[i][j]='-';
+		}
+	}
+	pair<int,int>salida;
+	queue<Estado>q;
+	bool visited[h][w];
+	for(int i=0;i<h; i++){
+		for(int j=0; j<w;j++){
+			visited[i][j]=0;
+		}
+	}
+	Estado inicial(x,y,0);
+	q.push(inicial);
+	prev[inicial.x][inicial.y]='n';
+	
+	int dx[]={-1,1,0,0};
+	int dy[]={0,0,-1,1};
+	
+	bool enc = false;
+	int pasosFinales;
+	
+	while(!q.empty() and !enc){
+		Estado actual = q.front();
+		q.pop();
+		if(adj[actual.x][actual.y]=='S'){
+			salida = make_pair(actual.x,actual.y);
+			enc = true;
+			pasosFinales = actual.d;
+		}
+		visited[actual.x][actual.y]=true;
+		for(int j=0; j<4; j++){
+			int nx = actual.x+dx[j];
+			int ny = actual.y+dy[j];
+			if(nx>=0 and nx<h and ny>=0 and ny<w and adj[nx][ny]!='#' and !visited[nx][ny]){
+				Estado vecino(nx,ny,actual.d+1);
+				q.push(vecino);
+				switch(j){
+					case 0:prev[vecino.x][vecino.y]='a';break;
+					case 1:prev[vecino.x][vecino.y]='A';break;
+					case 2:prev[vecino.x][vecino.y]='d';break;
+					case 3:prev[vecino.x][vecino.y]='i';break;
+				}
+			}
+		}
+	}
+	if(enc){makePath(salida,prev,pasosFinales);return pasosFinales;}
+	return -1;
 }
